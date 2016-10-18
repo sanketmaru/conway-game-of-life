b62458c4ea3562298b74caddb36f42c96490990f
sanketmaru <sanketmaru@gmail.com>
2016-10-18 23:16:42 +0530

    added pseudo code for identifying adjacent cells

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # conway-game-of-life
 conway-game-of-life
 
+This is a solution to the problem statement on freecodecamp (https://www.freecodecamp.com/challenges/build-the-game-of-life)
 
 # Game Rules defined here - (https://www.math.cornell.edu/~lipa/mec/lesson6.html)
+
+1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+2. Any live cell with two or three live neighbours lives on to the next generation.
+3. Any live cell with more than three live neighbours dies, as if by over-population.
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
 # Installation Guide
   1. Install node js
   2. npm install
-  3. npm start 
+  3. npm start
--- src/grid.js
+++ src/grid.js
@@ -22,7 +22,7 @@ class GridComponent extends Component {
         console.log("You clicked on col:",col);
         console.log("You clicked on item #:",i);
 
-        el.className='clicked';
+        el.className= el.className === 'clicked' ? 'unclicked' : 'clicked';
 
         lastClicked = el;
     });
@@ -38,16 +38,46 @@ class GridComponent extends Component {
         var grid = document.createElement('table');
         grid.className = 'grid';
         grid.id = 'grid';
-        for (var r=0;r<rows;++r){
+        for (var row = 0; row < rows; ++row){
             var tr = grid.appendChild(document.createElement('tr'));
-            for (var c=0;c<cols;++c){
-                var cell = tr.appendChild(document.createElement('td'));
+            for (var col = 0; col < cols; ++col){
+                var td = document.createElement('td');
+                td.id = row + "" + col;
+
+                // pseudo code :-
+
+                // 1. From current row select the parent row and the next row and each cell will have a unique id.
+                // 2. From the id , deduct the no of cols to reach to its adjacent top , -1 to left top diagonal , +1 to right top diagonal.
+                // 3. Frm the id in #1 , add the no of cols to reach to its adj btm , -1 to left btm diag, +1 to right btm diag.
+                // 4. repeat this process for all the cells at the same time and generate a new board and remove the existing one.
+                // 4.a store the existing one in pouchdb so history of generations can be shown , from pouch store to couch. // offline storeage.
+                
+                var parentArr = [];
+                var siblingsArr = [];
+                var childrensArr = [];
+                var parentRow, childRow;
+
+                if(row > 0) {
+                  parentRow = row - 1;
+                  childRow = row + 1;
+                }
+
+                for(var i=0 ;i<=col;i++) {
+
+                }
+
+                var neighbours = {
+                  parent : [], // diagonal , top
+                  siblings : [], // sides
+                  childrens : [] // diagonal , bottom
+                }
+                var cell = tr.appendChild(td);
                 ++i;
-                cell.addEventListener('click',(function(el,r,c,i){
+                cell.addEventListener('click',(function(el,row,col,i){
                     return function(){
-                        callback(el,r,c,i);
+                        callback(el,row,col,i);
                     }
-                })(cell,r,c,i),false);
+                })(cell,row,col,i),false);
             }
         }
         return grid;
